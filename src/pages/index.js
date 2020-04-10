924a6bc2732e7c781ffbd5a1d2bf711163945b61
José Sousa <josesousa9000@gmail.com>
2020-04-10 22:05:42 +0100

    Forestry update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 
+import homeData from "root/data/pages/index.json";
+
 import Layout from "root/components/Layout";
 import Circle from "root/assets/circle.svg?sprite";
 
 export default function HomePage() {
   return (
     <Layout>
-      <h1>MinhoCovid19</h1>
+      <h1>{homeData.title}</h1>
 
       <Circle />
     </Layout>
