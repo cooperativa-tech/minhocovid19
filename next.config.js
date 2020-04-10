eb38572a42d944f283ffcd35bc6e17d902e721ad
José Sousa <josesousa9000@gmail.com>
2020-04-10 21:30:56 +0100

    Add sprite svg loader

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-module.exports = {
+const withOptimizedImages = require("next-optimized-images");
+
+module.exports = withOptimizedImages({
   experimental: {
     jsconfigPaths: true,
   },
-};
+});
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 import Layout from "root/components/Layout";
+import Circle from "root/assets/circle.svg?sprite";
 
 export default function HomePage() {
   return (
     <Layout>
       <h1>MinhoCovid19</h1>
+
+      <Circle />
     </Layout>
   );
 }
