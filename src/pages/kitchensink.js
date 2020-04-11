e5d05b7e32e381eb7563d5058cc8d78b692a3f00
José Sousa <josesousa9000@gmail.com>
2020-04-11 12:37:50 +0100

    Add meta info to cms files

--- src/pages/kitchensink.js
+++ src/pages/kitchensink.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Layout from "root/components/Layout";
 
-import Layout from "../components/Layout";
+import content from "cms/pages/kitchensink.json";
 
 export default () => (
-  <Layout>
+  <Layout
+    title={content.title}
+    description={content.description}
+    keywords={content.keywords}
+  >
     Hello world
   </Layout>
-)
+);
