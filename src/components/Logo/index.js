f3d448144795205afecbea6f565b0c701960faca
João Justo <joaojusto@users.noreply.github.com>
2020-04-11 13:52:14 +0200

    Add logo component (#7)
    * Add logo component
    
    Why:
    
    * We need a way to render the logo
    
    This change addresses the need by:
    
    * Adding a new component named `Logo`;
    * Adding `<Logo />` to `kitchensink`;

---
+++ src/components/Logo/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Proptypes from "prop-types"
+
+import Sprite from "root/assets/logo.svg?sprite";
+
+const Logo = ({ width }) => (
+  <Sprite width={width} />
+);
+
+Logo.propTypes = {
+  width: Proptypes.number
+}
+
+Logo.defaultProps = {
+  width: 140
+}
+
+export default Logo;
