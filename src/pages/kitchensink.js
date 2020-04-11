a75e04acebbb815f12e9a0da1d3be8fe5997d8b8
Ricardo Martins <ricardofilipe5@sapo.pt>
2020-04-11 16:30:06 +0100

    Add tite component
    Why:
    
    * We need a reusable component for title component
    * We need three color themes and two sizes (h1,h2) for the title component;
    
    This change addresses the need by:
    
    * Adding title component;
    * Adding <Ttitle /> to kitchensink;

--- src/pages/kitchensink.js
+++ src/pages/kitchensink.js
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "root/components/Layout";
 
 import content from "cms/pages/kitchensink.json";
+import Title from "root/components/Title";
 import Logo from "../components/Logo";
 
 export default () => (
@@ -10,6 +11,9 @@ export default () => (
     description={content.description}
     keywords={content.keywords}
   >
+    <Title theme="redTheme">Juntos paramos o Covid19</Title>
+    <Title theme="neonCarrotTheme">Juntos paramos o Covid19</Title>
+    <Title theme="greyishBlueTheme">Juntos paramos o Covid19</Title>
     <Logo />
   </Layout>
 );
