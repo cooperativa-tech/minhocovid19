e9cedb863921d4ec35c790295e4e12b7f45846b3
José Sousa <josesousa9000@gmail.com>
2020-04-12 17:23:18 +0100

    Update

--- src/components/Subtitle/index.js
+++ src/components/Subtitle/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import classnames from "classnames";
 
 import styles from "./index.module.css";
 
@@ -16,7 +17,7 @@ export const THEMES = {
 };
 
 const Subtitle = ({ children, theme }) => {
-  const className = `${styles.root} ${theme}`;
+  const className = classnames(styles.root, theme);
 
   return <h2 className={className}>{children}</h2>;
 };
