12d4c4ca2b03ddddddf08a87f1372eb959606d24
Francisco Sousa <josesousa9000@gmail.com>
2020-05-08 12:18:08 +0100

    Add hi-fi transparency report

--- src/pages/faqs.js
+++ src/pages/faqs.js
@@ -4,6 +4,10 @@ import content from "cms/pages/faqs.json";
 import Layout from "root/components/Layout";
 import Navbar from "root/components/Navbar";
 import Faq from "root/components/Faq";
+import Section from "root/components/Section";
+import Title from "root/components/Title";
+import Themes from "root/components/Themes";
+import TextArea from "root/components/TextArea";
 
 import styles from "./faqs.module.css";
 
@@ -15,6 +19,16 @@ export default function FaqsPage() {
       keywords={content.keywords}
     >
       <Navbar />
+
+      <div className={styles.hero}>
+        <Section theme={Themes.linen}>
+          <div className={styles.heroTitle}>
+            <Title theme={Themes.neonCarrot}>{content.heroTitle}</Title>
+          </div>
+
+          <TextArea theme={Themes.goldYellow}>{content.heroCopy}</TextArea>
+        </Section>
+      </div>
 
       <div className={styles.faqs}>
         {content.faqs.map((faq, index) => (
--- src/pages/transparency.js
+++ src/pages/transparency.js
@@ -1,11 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import content from "cms/pages/transparency.json";
+import Navbar from "root/components/Navbar";
 import Layout from "root/components/Layout";
-
 import getTransparencyReport from "root/lib/getTransparencyReport";
+import Section from "root/components/Section";
+import TextArea from "root/components/TextArea";
+import Text from "root/components/Text";
+import Title from "root/components/Title";
+import Themes from "root/components/Themes";
 
-import content from "cms/pages/index.json";
+import styles from "./transparency.module.css";
 
 export default function HomePage({ transparencyReport }) {
   return (
@@ -14,24 +20,45 @@ export default function HomePage({ transparencyReport }) {
       description={content.description}
       keywords={content.keywords}
     >
-      <table style={{ marginTop: "300px" }}>
-        <thead>
-          <tr style={{ textAlign: "left" }}>
-            <th>Propósito</th>
-            <th>Data</th>
-            <th>Valor</th>
-          </tr>
-        </thead>
-        <tbody>
-          {transparencyReport.map((reportLine, index) => (
-            <tr key={index}>
-              <td>{reportLine.purpose}</td>
-              <td>{reportLine.date}</td>
-              <td>{reportLine.value}€</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <Navbar />
+
+      <div className={styles.hero}>
+        <Section theme={Themes.lightBlue}>
+          <div className={styles.heroTitle}>
+            <Title theme={Themes.blue}>{content.heroTitle}</Title>
+          </div>
+
+          <TextArea theme={Themes.greyishBlue}>{content.heroCopy}</TextArea>
+        </Section>
+      </div>
+
+      <div className={styles.table}>
+        <div className={styles.tableHeader}>
+          <th className={styles.dateCol}>
+            <Text theme={Themes.greyishBlue}>Data</Text>
+          </th>
+          <th className={styles.purposeCol}>
+            <Text theme={Themes.greyishBlue}>Propósito</Text>
+          </th>
+          <th className={styles.valueCol}>
+            <Text theme={Themes.greyishBlue}>Valor</Text>
+          </th>
+        </div>
+
+        {transparencyReport.map((reportLine, index) => (
+          <span key={index} className={styles.tableRow}>
+            <div className={styles.dateCol}>
+              <Text theme={Themes.blue}>{reportLine.date}</Text>
+            </div>
+            <div className={styles.purposeCol}>
+              <Text theme={Themes.blue}>{reportLine.purpose}</Text>
+            </div>
+            <div className={styles.valueCol}>
+              <Text theme={Themes.blue}>{reportLine.value}€</Text>
+            </div>
+          </span>
+        ))}
+      </div>
     </Layout>
   );
 }
