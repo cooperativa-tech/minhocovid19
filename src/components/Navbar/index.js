e6649a2171901f17aae85bc71da2f1193af425d4
José Sousa <josesousa9000@gmail.com>
2020-05-10 01:49:40 +0100

    HOTFIX: Fix how to help link

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -67,7 +67,7 @@ const Navbar = ({ home }) => {
             <NavLink href="/transparency">{content.transparency}</NavLink>
           </li>
           <li className={styles.link}>
-            <NavButton href="#howtohelp">{content.cta}</NavButton>
+            <NavButton href="/#howtohelp">{content.cta}</NavButton>
           </li>
         </ul>
       </div>
--- src/components/Navbar/overlay.js
+++ src/components/Navbar/overlay.js
@@ -58,7 +58,7 @@ const Overlay = ({ isOpen, onClose }) => (
       </div>
     </div>
 
-    <Button theme={Themes.white} href="#howtohelp" onClick={onClose}>
+    <Button theme={Themes.white} href="/#howtohelp" onClick={onClose}>
       {content.cta}
     </Button>
   </div>
